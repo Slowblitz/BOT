9ceafe649173a86a99a7049c3163019814ecbeac
slowblitz <jeremygarcia937@gmail.com>
2020-11-09 18:16:30 +0100

    First commit

---
+++ actions.py
@@ -0,0 +1,18 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from rasa_sdk.interfaces import Action
+
+class ActionGreet(Action):
+    def name(self):
+        return 'action_greet'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(template="utter_greet")
+        return []
