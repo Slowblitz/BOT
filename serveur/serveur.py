b5be3a7bb578beb4fe8bb19d26c544603cfa5689
jvallot <jvallot@gdsoft.fr>
2020-12-08 17:14:45 +0100

    - récupération message de choregraphe

--- serveur/serveur.py
+++ serveur/serveur.py
@@ -1,19 +1,26 @@
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request
 import requests
+import json
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-#Changer
-ip = "192.168.1.28"
+# Changer
+ip = "10.122.4.166"
 api = "http://" + ip + ":5005/webhooks/rest/webhook"
+
 
 @app.route('/')
 def hello_world():
-        return 'Hello, World!'
+    return 'Hello, World!'
 
-@app.route('/GetResponseRasa')
+
+@app.route('/GetResponseRasa', methods=['GET', 'POST'])
 def getResponseRasa():
-        core = {"sender":"rasa", "message":"Donne moi mon emploi du temps"}
-        response = requests.post(api, json=core)
-        return jsonify(response.json())
+    if request.method == 'POST':
+        datas = json.loads(request.data)
+        message = datas['text']
+        print(message)
+    core = {"sender": "rasa", "message": message}
+    response = requests.post(api, json=core)
+    return jsonify(response.json())
